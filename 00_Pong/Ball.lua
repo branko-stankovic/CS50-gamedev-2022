f39b577c5b8fc684cfdee5221673d69e4925403c
branko-stankovic <branko.stankovic@gmail.com>
2022-05-18 17:14:16 +0200

    Fixed ball initial speed, to serve in the proper direction

--- 00_Pong/Ball.lua
+++ 00_Pong/Ball.lua
@@ -18,8 +18,8 @@ function Ball:init(x, y, width, height)
 
     -- these variables are for keeping track of our velocity on
     -- X and Y axis, since the ball can move in two dimensions
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50)
 end
 
 --[[
@@ -28,8 +28,8 @@ end
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
-    self.dy = math.random(2) == 1 and -100 or 100
-    self.dx = math.random(-50, 50)
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy = math.random(-50, 50)
 end
 
 --[[
