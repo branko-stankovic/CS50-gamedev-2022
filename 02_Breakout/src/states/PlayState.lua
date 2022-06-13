dfff6010cbee67afde87b25ff7b12cdb375efa5e
branko-stankovic <branko.stankovic@gmail.com>
2022-06-13 04:47:50 +0200

    Breakout-8 done

--- 02_Breakout/src/states/PlayState.lua
+++ 02_Breakout/src/states/PlayState.lua
@@ -148,6 +148,11 @@ function PlayState:update(dt)
         end
     end
 
+    -- for rendering particle systems
+    for k, brick in pairs(self.bricks) do
+        brick:update(dt)
+    end
+
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
@@ -157,6 +162,11 @@ function PlayState:render()
     -- render bricks
     for k, brick in pairs(self.bricks) do
         brick:render()
+    end
+
+    -- render all particle systems
+    for k, brick in pairs(self.bricks) do
+        brick:renderParticles()
     end
 
     self.paddle:render()
